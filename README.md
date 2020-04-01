ac639f59acec92489725175f132d55c6efa45746
Camille <c.hughes64@gmail.com>
2020-04-01 11:21:08 -0400

    heroku issues

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Following Fullstack Udemy Course
 <a href="https://www.udemy.com/course/node-with-react-fullstack-web-development/">Node with React: Full-Stack Web Development</a> by <a href="https://github.com/StephenGrider">Stephen Grider</a>
+
+Deployed to <a href="https://feedback-collection-1.herokuapp.com/">Heroku</a>
--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,9 @@
 //PROD.JS
 module.exports = {
-    googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    facebookClientID: process.env.FACEBOOK_CLIENT_ID,
-    facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-    mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  facebookClientID: process.env.FACEBOOK_CLIENT_ID,
+  facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
 };
