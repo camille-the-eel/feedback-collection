52f13469ad8a793eb49b06aa34c28c9caa873ddb
Camille <c.hughes64@gmail.com>
2020-04-05 16:11:30 -0400

    sass bootstrap 7-1 file structure basic setup

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 // BrowserRouter: "the brain" React Component, tells react-router how to behave, looks at the current URL and then changes the set of visible components on the page at any given time
 // Route: React Component used to set up a rule between a certain route that the user might visit and a set of components that will actually be visible on the screen
+import './stylesheets/App.scss';
+import Header from './components/Header';
 
 // dumby components for route set up
-const Header = () => <h2>Header</h2>
 const Dashboard = () => <h2>Dashboard</h2>
-const SurveryNew = () => <h2>Survery New</h2>
+const SurveyNew = () => <h2>Survery New</h2>
 const Landing = () => <h2>Landing</h2>
+
+
 
 // BrowserRouter only takes one child (so everything needs to be wrapped in one div)
 
@@ -16,7 +19,10 @@ const App = () => {
         <div>
             <BrowserRouter>
                 <div>
-                    <Route path="/" component={Landing} />
+                    <Header />
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/surveys" component={Dashboard} />
+                    <Route path="/surveys/new" component={SurveyNew} />
                 </div>
             </BrowserRouter>
         </div>
