f6119ab161c6aee562664ce23c59d35f1c76fdaa
Camille <c.hughes64@gmail.com>
2020-04-06 15:31:12 -0400

    connect Header component to redux and test props

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
   render() {
+    //test connect/state
+      console.log(this.props);
     return (
       <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -20,4 +23,16 @@ class Header extends Component {
   }
 }
 
-export default Header;
+// called with the entire state object out of the redux store
+// then we return which branches of it we want -- see reducers/index for where we defined the keys (branches)
+
+// function mapStateToProps(state) {
+//     return { auth: state.auth };
+// }  
+
+// refactored with destructuring, but so you remember ^^^ that is what is happening there
+function mapStateToProps({ auth }) {
+    return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
