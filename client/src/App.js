f48df1743206cebf583503cc476545c355e793d4
Camille <c.hughes64@gmail.com>
2020-04-06 13:27:47 -0400

    header component and base sass

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
+import './stylesheets/App.scss';
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 // BrowserRouter: "the brain" React Component, tells react-router how to behave, looks at the current URL and then changes the set of visible components on the page at any given time
 // Route: React Component used to set up a rule between a certain route that the user might visit and a set of components that will actually be visible on the screen
-import './stylesheets/App.scss';
+
 import Header from './components/Header';
 
 // dumby components for route set up
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,21 @@ import React, { Component } from 'react';
 
 class Header extends Component {
   render() {
-    return <div>header</div>;
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">
+            Emaily
+          </a>
+          <a className="nav-item nav-link" href="#">
+            Login with Google
+          </a>
+          <a className="nav-item nav-link" href="#">
+            Login with Facebook
+          </a>
+        </nav>
+      </div>
+    );
   }
 }
 
