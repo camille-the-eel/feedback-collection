ccc6290aaacdd5691001df24413196c420af9004
Camille <c.hughes64@gmail.com>
2020-04-11 17:37:06 -0400

    altering style of default Stripe button

--- client/src/components/StripeWrapper.js
+++ client/src/components/StripeWrapper.js
@@ -10,7 +10,11 @@ class StripeWrapper extends Component {
         amount={500}
         token={(token) => console.log(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+          <button className="btn">
+              Add Credits
+          </button>
+      </StripeCheckout>
     );
   }
 }
